24d2778d68e70c1eb0bc9a33520e34204c4d1e9b
Ernst <Taylor.Ernst@QASOFFICE.com>
2019-02-12 14:13:56 -0800

    improved menu/blur performance.

--- public/a3.js
+++ public/a3.js
@@ -1,16 +1,16 @@
 (function () {
   let blurBehindNav = function (event) {
-    if (document.getElementById("assignment-container").style.height === "100%") {
+    if (document.getElementById("assignment-container").classList.contains("sidenav-show")) {
       if (this.id !== event.target.id && this.id !== "navbar-toggler-button") {
         event.stopPropagation();
         return;
       }
       document.getElementById("navbar-toggler-button").classList.remove("open");
-      document.getElementById("assignment-container").style.height = "0%";
+      document.getElementById("assignment-container").classList.remove("sidenav-show");
       document.querySelectorAll(".blurable").forEach(e => e.classList.remove("blur"));
     } else {
       document.getElementById("navbar-toggler-button").classList.add("open");
-      document.getElementById("assignment-container").style.height = "100%";
+      document.getElementById("assignment-container").classList.add("sidenav-show");
       document.querySelectorAll(".blurable").forEach(e => e.classList.add("blur"));
     }
   };
