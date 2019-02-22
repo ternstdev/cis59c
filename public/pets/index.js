377835e7f3ed25ba793b8a54b14c00e087890e71
Taylor E <taylor.ernst@hotmail.com>
2019-02-21 22:52:45 -0800

    Added pets

---
+++ public/pets/index.js
@@ -0,0 +1,20 @@
+(function () {
+  let blurBehindNav = function (event) {
+    if (document.getElementById("assignment-container").classList.contains("sidenav-show")) {
+      if (this.id !== event.target.id && this.id !== "navbar-toggler-button") {
+        event.stopPropagation();
+        return;
+      }
+      document.getElementById("navbar-toggler-button").classList.remove("open");
+      document.getElementById("assignment-container").classList.remove("sidenav-show");
+      document.querySelectorAll(".blurable").forEach(e => e.classList.remove("blur"));
+    } else {
+      document.getElementById("navbar-toggler-button").classList.add("open");
+      document.getElementById("assignment-container").classList.add("sidenav-show");
+      document.querySelectorAll(".blurable").forEach(e => e.classList.add("blur"));
+    }
+  };
+
+  document.getElementById("navbar-toggler-button").addEventListener("click", blurBehindNav);
+  document.getElementById("assignment-container").addEventListener("click", blurBehindNav);
+})();
