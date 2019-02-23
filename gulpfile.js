02c768e219ca7264dda9d8db2aebb46f7c530c20
Taylor E <taylor.ernst@hotmail.com>
2019-02-23 15:15:50 -0800

    Switched over to SCSS and updated primary-color.

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+// Sass configuration
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function (cb) {
+  gulp.src('./scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./public/assets'))
+    .pipe(gulp.dest(function (f) {
+      return f.base;
+    }));
+  cb();
+});
+
+gulp.task('default', gulp.series('sass', function (cb) {
+  gulp.watch('./scss/*.scss', gulp.series('sass'));
+  cb();
+}));
