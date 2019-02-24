83063ae46bdd05b31acb0a759988a65452b843ef
Taylor E <taylor.ernst@hotmail.com>
2019-02-24 13:22:48 -0800

    Fixed route for API.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -20,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(subdomain('api', router));
+app.use(subdomain('api', apiRouter));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
