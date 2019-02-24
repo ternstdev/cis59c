a15654e0924ef3d08face03db17978bf9ba2b232
Taylor E <taylor.ernst@hotmail.com>
2019-02-24 15:32:27 -0800

    Fixed logger.

--- middleware/customlogger.js
+++ middleware/customlogger.js
@@ -1,8 +1,8 @@
-const fs = requrie('fs');
+const fs = require('fs');
 
 const customlogger = (req, res, next) => {
   const logFile = fs.openSync('./public/log.txt', 'a');
-  fs.writeSync(logFile, `${req.protocol}://${req.get('host')}${req.originalUrl}`, null, null);
+  fs.writeSync(logFile, `${req.protocol}://${req.get('host')}${req.originalUrl}\n`, null, null);
   next();
 };
 
