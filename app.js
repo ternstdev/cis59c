b42b0395752157ef708b9b1ce0fcdf1978c080c8
Taylor E <taylor.ernst@hotmail.com>
2019-02-24 16:27:28 -0800

    Debugging failure to launch.

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var createError = require('http-errors');
-var subdomain = require('express-subdomain');
+//var subdomain = require('express-subdomain');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-//var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var apiRouter = require('./routes/api');
+//var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -21,10 +21,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(subdomain('api', apiRouter));
+//app.use(subdomain('api', apiRouter));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', indexRouter);
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
