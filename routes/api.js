6d7198df03f1a706504bd921a2dc6283cbfaa84d
Taylor E <taylor.ernst@hotmail.com>
2019-02-24 16:42:40 -0800

    Updated allPets.

--- routes/api.js
+++ routes/api.js
@@ -7,44 +7,50 @@ router.get('/', function (req, res) {
 }); */
 
 router.get('/pets/all', function (req, res, next) {
-  let allPets = {
-    0: {
+  const allPets = [
+    {
+      id: 0,
       name: "Rocky",
       breed: "Terrier Mix",
       shortDesc: "Rocky is a friendly, well behaved pup and the perfect companion for long walks!",
-      imgs: { 0: "./img/rockygrizzled.jpg" },
+      imgs: ["./img/rockygrizzled.jpg"],
     },
-    1: {
+    {
+    id: 1,
       name: "Scout",
       breed: "Terrier Mix",
       shortDesc: "Scout is a spunky terrier who loves nothing more than taking a nap on your lap!",
-      imgs: { 0: "./img/scoutportrait.jpg" },
+      imgs: ["./img/scoutportrait.jpg"],
     },
-    2: {
+    {
+    id: 2,
       name: "Teddy",
       breed: "Terrier Mix",
       shortDesc: "Teddy loves to play fetch and jam to his <a href=\"https://www.youtube.com/watch?v=ci_pCk0l-WA\">favorite song</a>!",
-      imgs: { 0: "./img/teddy.jpg" },
+      imgs: ["./img/teddy.jpg"],
     },
-    3: {
+    {
+    id: 3,
       name: "Badger",
       breed: "Cool Guy",
       shortDesc: "When he's not making funny faces, Badger can most often be found curled up under a pile of blankets.",
-      imgs: { 0: "./img/badger.jpg" },
+      imgs: ["./img/badger.jpg"],
     },
-    4: {
+    {
+    id: 4,
       name: "Hedgy",
       breed: "Hedgehog",
       shortDesc: "Don't let his spikey exterior fool you — Hedgy the Hedgehog is full of love!",
-      imgs: { 0: "./img/hedgy.jpg" },
+      imgs: ["./img/hedgy.jpg"],
     },
-    5: {
+    {
+    id: 5,
       name: "Emily",
       breed: "Formosan Mountain Dog",
       shortDesc: "Emily is wary of new people, but give her a few treats, and she'll warm up to you!",
-      imgs: { 0: "./img/emily.jpg" },
+      imgs: ["./img/emily.jpg"],
     },
-  };
+  ];
   
   res.json(allPets);
   
