4080b17dc90d100b99c0da0aff20af7855793b40
Ernst <Taylor.Ernst@QASOFFICE.com>
2019-02-25 17:30:02 -0800

    Added an example of an accordion show/hide function for future reference

--- public/pets/index.js
+++ public/pets/index.js
@@ -18,3 +18,43 @@
   document.getElementById("navbar-toggler-button").addEventListener("click", blurBehindNav);
   document.getElementById("assignment-container").addEventListener("click", blurBehindNav);
 })();*/
+
+
+/*
+
+//Here's an example of a 'click' event handler to expand/collapse an accordion:
+// Source: https://healthy.kaiserpermanente.org/northern-california/facilities/Pleasanton-Medical-Offices-100340
+
+
+                getPanel: function(t) {
+                    return document.getElementById(t.getAttribute("aria-controls"))
+                },
+                show: function(t) {
+                    if (this.getPanel(t).setAttribute("aria-hidden", "false"),
+                    this.getPanel(t).classList.add("show"),
+                    t.setAttribute("aria-expanded", "true"),
+                    !n) {
+                        var e = void 0 !== window.pageYOffset ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop
+                          , i = t.offsetTop;
+                        e > i && window.scrollTo(0, i)
+                    }
+                },
+                hide: function(t) {
+                    this.getPanel(t).setAttribute("aria-hidden", "true"),
+                    t.setAttribute("aria-expanded", "false"),
+                    this.getPanel(t).classList.remove("show")
+                },
+                hideAll: function() {
+                    this.tabs.map(function(t) {
+                        this.hide(t)
+                    }, this)
+                },
+                onTabClick: function(t) {
+                    var e = "true" !== t.target.getAttribute("aria-expanded");
+                    this.isMultiple || this.hideAll(),
+                    e ? this.show(t.target) : this.isMultiple && this.tabs.map(function(e) {
+                        e.getAttribute("id") === t.target.getAttribute("id") && this.hide(e)
+                    }, this)
+                },
+
+*/
