585971db497ed04b6cb29e3decfd4855314bba54
Taylor E <taylor.ernst@hotmail.com>
2019-04-11 21:18:01 -0700

    Debugging db animal creation.

--- routes/api.js
+++ routes/api.js
@@ -16,7 +16,7 @@ const canParseInt = (text, min, max) => {
 const validateInput = (req) => {
   let i = -1;
   if (req.param("id")) {
-    if (canParseInt(req.param("id"), 0, 9999999)) {
+    if (!canParseInt(req.param("id"), 0, 9999999)) {
       return false;
     }
   }
@@ -24,7 +24,7 @@ const validateInput = (req) => {
     return false;
   }
   
-  if (canParseInt(req.param("typeId"), 0, 30)) {
+  if (!canParseInt(req.param("typeId"), 0, 30)) {
     return false;
   }
   
@@ -32,7 +32,7 @@ const validateInput = (req) => {
     return false;
   }
   
-  if (canParseInt(req.param("age"), 0, 200)) {
+  if (!canParseInt(req.param("age"), 0, 200)) {
     return false;
   }
   
@@ -40,30 +40,30 @@ const validateInput = (req) => {
     return false;
   }
   
-  if (canParseInt(req.param("houseTrained"), 0, 5)) {
+  if (!canParseInt(req.param("houseTrained"), 0, 5)) {
     return false;
   }
   
   
-  if (canParseInt(req.param("specialNeeds"), 0, 5)) {
+  if (!(req.param("specialNeeds"), 0, 5)) {
     return false;
   }
-  if (canParseInt(req.param("energy"), 0, 5)) {
+  if (!canParseInt(req.param("energy"), 0, 5)) {
     return false;
   }
-  if (canParseInt(req.param("affection"), 0, 5)) {
+  if (!canParseInt(req.param("affection"), 0, 5)) {
     return false;
   }
-  if (canParseInt(req.param("obedience"), 0, 5)) {
+  if (!canParseInt(req.param("obedience"), 0, 5)) {
     return false;
   }
-  if (canParseInt(req.param("children"), 0, 5)) {
+  if (!canParseInt(req.param("children"), 0, 5)) {
     return false;
   }
-  if (canParseInt(req.param("strangers"), 0, 5)) {
+  if (!canParseInt(req.param("strangers"), 0, 5)) {
     return false;
   }
-  if (canParseInt(req.param("otherAnimals"), 0, 5)) {
+  if (!canParseInt(req.param("otherAnimals"), 0, 5)) {
     return false;
   }
   
