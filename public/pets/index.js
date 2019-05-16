c5bc7f5eb1fb4e3ce6bb2a3959327773fac5f030
Taylor E <taylor.ernst@hotmail.com>
2019-05-16 00:50:45 -0700

    Learn More button now creates a DOM table.

--- public/pets/index.js
+++ public/pets/index.js
@@ -8,7 +8,10 @@ let navbarToggler = function () {
 document.getElementById("nav-menu-button").addEventListener("click", navbarToggler);
 document.getElementById("nav-menu-closer").addEventListener("click", navbarToggler);
 
-
+const ATTRIBUTE_LIST = [
+  "id", "name", "typeId", "breed", "age",
+  "shortDesc", "houseTrained", "specialNeeds", "energy", "affection",
+  "obedience", "children", "strangers", "otherAnimals", "imgs"];
 
 // Setup numeric inputs
 // data-numtype options:
@@ -218,7 +221,7 @@ function formSubmitNewAnimal(evt) {
 
 
 if (document.getElementById("main-grid")) {
-  fetch('https://api.tay.fail/pets/animals') // Initiates the request.
+  fetch('https://api.ernst.dev/pets/animals') // Initiates the request.
     .then(function (response) { // Once we receive a response, run the function below.
       var animals = response.json() // Converts the response from JSON into an object or array (in our case, an array).
       return animals;
@@ -231,9 +234,13 @@ function displayAnimals(animals) {
   animals.forEach((animal) => {
     let node = document.createElement("article");
     node.classList.add("card");
+    for (const attribute of ATTRIBUTE_LIST) {
+      node.setAttribute('data-' + attribute, animal[attribute]);
+    }
+
     // old image: <img src="./img/${animal.imgs[0]}" alt="${animal.name}" class="img-responsive hover-zoom" />
     // new image: <img alt="${animal.name}" class="img-responsive hover-zoom" style="height: 0; padding-top: 56.25%; background: url(./img/${animal.imgs[0]}); background-size: cover; background-repeat: no-repeat;">
-    node.innerHTML = '<a class="card-image" href="#">' +
+    node.innerHTML = '<a class="card-image learn-more" name="' + animal.id + '" href="#">' +
       '<div alt="' + animal.name + '" class="img-aspect hover-zoom" style="background: url(./img/' + animal.imgs[0] + '); background-size: cover; background-repeat: no-repeat;"></div>' +
       '</a>' +
       '<header class="card-header">' +
@@ -244,13 +251,100 @@ function displayAnimals(animals) {
       animal.shortDesc +
       '</p>' +
       '<footer class="card-footer">' +
-      '<a class="btn btn-primary" href="#">Learn more!</a>' +
+      '<a class="btn btn-primary learn-more" name="' + animal.id + '" href="#">Learn more!</a>' +
       '<span class="float-right m-2 text-gray text-italic">' + (animal.matchness ? animal.matchness : "") + '</span>' +
       '</footer>';
     document.getElementById("main-grid").appendChild(node);
   });
-}
+  document.querySelectorAll(".learn-more").forEach(function (learnMore) {
+    learnMore.addEventListener("click", function (evt) {
+      const animalId = this.name;
+      let parentCard = document.querySelector('[data-id="' + animalId + '"]');
+      writeDomTable(parentCard);
 
+    });
+  });
+};
+
+function writeDomTable(animalCard) {
+  if (!animalCard)
+    return;
+
+  let domTable = '<table class="table">' +
+    '<thead>' +
+    '<tr>' +
+    '<th>Property</th>' +
+    '<th>Value</th>' +
+    '</tr>' +
+    '</thead>' +
+    '<tbody>' +
+    '<tr>' +
+    '<td>Name</td>' +
+    '<td id="name_value">' + animalCard.getAttribute("data-name") + '</td>' +
+    '</tr>' +
+    '<tr>' +
+    '<td>Energy Level</td>' +
+    '<td id="energy_value">' + animalCard.getAttribute("data-energy") + '</td>' +
+    '</tr>' +
+    '<tr>' +
+    '<td>Affection Level</td>' +
+    '<td id="affection_value">' + animalCard.getAttribute("data-affection") + '</td>' +
+    '</tr>' +
+    '<tr>' +
+    '<td>Obedience</td>' +
+    '<td id="obedience_value">' + animalCard.getAttribute("data-obedience") + '</td>' +
+    '</tr>' +
+    '<tr>' +
+    '<td>Children</td>' +
+    '<td id="children_value">' + animalCard.getAttribute("data-children") + '</td>' +
+    '</tr>' +
+    '<tr>' +
+    '<td>Strangers</td>' +
+    '<td id="strangers_value">' + animalCard.getAttribute("data-strangers") + '</td>' +
+    '</tr>' +
+    '<tr>' +
+    '<td>Other Animals</td>' +
+    '<td id="other_animals_value">' + animalCard.getAttribute("data-otherAnimals") + '</td>' +
+    '</tr>' +
+    '</tbody>' +
+    '</table>';
+
+  let modalCard =
+    '<a href="#close" class="modal-overlay close-modal" aria-label="Close"></a>' +
+    '<div class="modal-container">' +
+    '<div class="modal-header">' +
+    '<a href="#close" class="btn btn-clear float-right close-modal" aria-label="Close"></a>' +
+    '<div class="modal-title h5">' + animalCard.getAttribute("data-name") + '</div>' +
+    '</div>' +
+    '<div class="modal-body">' +
+    '<div class="content">' +
+    '<div class="card">' +
+    '<div class="card-image img">' +
+    '<div alt="' + animalCard.getAttribute("data-name") + '" class="img-aspect hover-zoom" style="background: url(./img/' + animalCard.getAttribute("data-imgs").split(",")[0] + '); background-size: cover; background-repeat: no-repeat;"></div>' +
+    '</div>' +
+    '</div>' +
+    domTable +
+    '</div>' +
+    '</div>' +
+    '</div>';
+
+
+  let modalContainer = document.getElementById("learn-more-modal");
+  modalContainer.innerHTML = modalCard;
+  modalContainer.classList.add("active");
+  let closeModalButtons = document.getElementsByClassName("close-modal");
+  for (let i = 0; i < closeModalButtons.length; ++i) {
+    closeModalButtons[i].addEventListener("click", function (evt) {
+      let modalContainer = document.getElementById("learn-more-modal");
+      modalContainer.classList.remove("active");
+      modalContainer.innerHTML = "";
+    });
+  }
+
+
+  //document.getElementById("learn-more-body").innerHTML = domTable;
+  //document.getElementById("learn-more-image").innerHTML = '<div alt="' + animal.name + '" class="img-aspect hover-zoom" style="background: url(./img/' + animal.imgs[0] + '); background-size: cover; background-repeat: no-repeat;"></div>';
+};
 
 
 
