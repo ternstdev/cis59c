174380315ee3e683a6ad4b344109749c3e324d2e
Ernst <Taylor.Ernst@QASOFFICE.com>
2019-05-31 12:14:01 -0700

    Updated success response for single image upload.

--- routes/api.js
+++ routes/api.js
@@ -537,7 +537,7 @@ router.post('/pets/images/:id', upload.single('img'), function (req, res, next) 
               res.status(400).send(error);
               throw error;
             }
-            res.status(201).json({ msg: "Success", isSuccess: true, id: results.insertId });
+            res.status(201).json({ msg: "Success", isSuccess: true});
           });
       //});
       //res.status(201).json({ msg: "Success", isSuccess: true});
